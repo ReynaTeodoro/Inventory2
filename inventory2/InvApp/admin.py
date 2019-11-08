5f164b394d6c4f742b7742a1b94b171a4d64b5c8
Diegodiego12 <diegoherrera77777@gmail.com>
2019-11-08 08:41:41 -0300

    cambios admin

--- inventory2/InvApp/admin.py
+++ inventory2/InvApp/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Objeto, Registro, Usuario, Laboratorio, Conjunto, Armario, Especialidad, Categoria
 # Register your models here.
 admin.site.register(Objeto)
-admin.site.register(Registro)
+
+
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'descripcion', 'usuario')
+admin.site.register(Registro, RegistroAdmin)
 admin.site.register(Usuario)
 admin.site.register(Laboratorio)
-admin.site.register(Conjunto)
+
+
+class ConjuntoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'stock', 'descripcion')
+admin.site.register(Conjunto, ConjuntoAdmin)
 admin.site.register(Armario)
 admin.site.register(Especialidad)
 admin.site.register(Categoria)
