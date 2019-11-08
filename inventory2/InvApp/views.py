a24342a005efe1f83778ec5867122c209ea5259d
edu <edufeli22@gmail.com>
2019-11-08 11:22:54 -0300

    Creamos templates con Bootstrap

--- inventory2/InvApp/views.py
+++ inventory2/InvApp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def home(req):
+    return render(req, "base.html")
