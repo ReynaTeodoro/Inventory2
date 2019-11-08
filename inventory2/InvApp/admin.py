58610c1360d01ee1586785c579cc0190c261ba9c
BoldiniAgustin <agustinboldini@gmail.com>
2019-11-08 08:55:14 -0300

    ConjuntoAdmin y RegistroAdmin + bugfix (devuelta)

--- inventory2/InvApp/admin.py
+++ inventory2/InvApp/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from .models import Objeto, Registro, Usuario, Laboratorio, Conjunto, Armario, Especialidad, Categoria
 # Register your models here.
-admin.site.register(Objeto)
+
 
 
 class RegistroAdmin(admin.ModelAdmin):
     list_display = ('fecha', 'descripcion', 'usuario')
-admin.site.register(Registro, RegistroAdmin)
-admin.site.register(Usuario)
-admin.site.register(Laboratorio)
 
 
 class ConjuntoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'stock', 'descripcion')
+
+admin.site.register(Objeto)
+admin.site.register(Registro, RegistroAdmin)
+admin.site.register(Usuario)
+admin.site.register(Laboratorio)
 admin.site.register(Conjunto, ConjuntoAdmin)
 admin.site.register(Armario)
 admin.site.register(Especialidad)
