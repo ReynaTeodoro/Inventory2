989575d8112a72316fca8a448091a9e5f12065dc
ReynaTeodoro <teo.dileo@gmail.com>
2019-11-29 06:54:09 -0300

    arreglado

--- inventory2/inventory2/settings.py
+++ inventory2/inventory2/settings.py
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'inventario2',
-=======
     'InvApp',
->>>>>>> develop
 ]
 
 MIDDLEWARE = [
